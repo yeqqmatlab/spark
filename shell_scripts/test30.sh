1c8f789cbbe1d1697b172fccfe66e2b52034228a
yeqqmatlab <1027123792@qq.com>
2019-03-20 11:11:00 +0800

    提交脚本

---
+++ shell_scripts/test30.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+MINPARAMS=10
+
+echo
+
+echo The name this script is $0
+
+echo "The name this script is \" `basename $0` \"."
+
+if [ -n "$1" ]; then
+echo "Parameter #1 is $1"
+fi
+
+if [ -n "${10}" ]; then
+echo "Parameter #10 is ${10}"
+
+fi
+
+echo "---------------------------------------"
+
+echo "a"
